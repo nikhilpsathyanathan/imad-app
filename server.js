188e2d80b53c1e426c04cef1ca51aec369e885b1
Nikhil ps <32610794+nikhilpsathyanathan@users.noreply.github.com>
2018-02-19 20:14:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',funcation(req,res){
+app.get('/article-one',function(req,res){
     res.send('article one requested will be served here');
 });
 
-app.get('/article-two',funcation(req,res){
+app.get('/article-two',function(req,res){
     res.send('article two requested will be served here');
 });
 
